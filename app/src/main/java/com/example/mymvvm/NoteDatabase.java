1fe10a7fa50e6442a8c676292629ee21b4f1cbba
Hovhannes <hovhannesg445@gmail.com>
2023-04-22 01:34:21 +0400

    Some errors

---
+++ app/src/main/java/com/example/mymvvm/NoteDatabase.java
@@ -0,0 +1,50 @@
+package com.example.mymvvm;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+
+@Database(entities = {Note.class},version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+    private static NoteDatabase instance;
+    public abstract NoteDao noteDao();
+    public static synchronized NoteDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class,"note_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulationDbAsyncTask(instance).execute();
+        }
+    };
+    private static class PopulationDbAsyncTask extends AsyncTask<Void,Void,Void>{
+        private NoteDao noteDao;
+        private PopulationDbAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1","Desc1",1));
+            noteDao.insert(new Note("Title2","Desc2",2));
+            noteDao.insert(new Note("Title3","Desc3",3));
+            return null;
+        }
+    }
+}
